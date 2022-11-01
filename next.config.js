72b1b5a38e29ad7c48fe505343e0673f1185b6fd
Roberto Cinetto <roberto.cinetto@gmail.com>
2022-10-31 19:00:06 -0700

    Fix: Heroku domain on images domain in NextJS config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   swcMinify: true,
   images: {
     loader: 'default',
-    domains: ['localhost'],
+    domains: ['localhost', 'robertocinetto-com-strapi.herokuapp.com'],
   },
 }
 
